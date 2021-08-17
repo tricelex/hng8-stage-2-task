3cc6526254cf74064158add7ecf232c3810987dc
Chuckz Okoye <tricelex@gmail.com>
2021-08-17 17:50:54 +0100

    change allowed hosts again

--- resume/settings.py
+++ resume/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = "/static/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
